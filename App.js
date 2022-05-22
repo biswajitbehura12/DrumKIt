f9e717edf3dd7284688fede4fa507870daacdc48
BISWAJIT BEHURA <102812316+biswajitbehura12@users.noreply.github.com>
2022-05-22 16:55:06 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,30 @@
 
+        window.addEventListener('keydown', function(e){
+    
+          //select corresponding audio
+          const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`)
+          //select corresponding key
+          const key = document.querySelector(`.key[data-key = "${e.keyCode}"]`)
+    
+          if (!audio){
+            return
+          }
+          audio.currentTime = 0 // rewind to the start
+          audio.play()
+          key.classList.add('playing')
+        
+        })
+    
+        const removeTransition = function(e){
+          if (e.propertyName !== 'transform')
+            return
+          this.classList.remove('playing')
+        }
+    
+      
+        const keys = document.querySelectorAll('.key')
+        console.log(keys)
+        keys.forEach(function(key){
+          key.addEventListener('transitionend', removeTransition)
+        })
+    
